439976139bc7a9205bb1edb2d0fa5fe0935dec5e
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-01-27 14:00:58 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -15,8 +15,13 @@ char size(int cms) {
 
 int main() {
     assert(size(37) == 'S');
+    assert(size(38) == 'S');//size 38 is undefined size
+    assert(size(-38) == 'S'); //size is a unsigned integer, doesnot cover the negative inputs
     assert(size(40) == 'M');
+    assert(size(42) == 'M'); //Size 42 is undefined size
     assert(size(43) == 'L');
+    assert(size(29) != 'S'); //No Small size lower limit is specified, unbounded input range
+	assert(size(43) != 'L'); //No Large size higher limit is specified, unbounded input range
     printf("All is well (maybe!)\n");
     return 0;
 }
