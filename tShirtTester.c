389ba340659059825bb4999c9324e4a28d03899f
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-02-01 21:17:49 +0530

    Create tShirtTester.c

---
+++ tShirtTester.c
@@ -0,0 +1,16 @@
+#include "tshirtTester.h"
+#include <assert.h>
+
+void testTshirtSize(int size_cms, char expectedSize) {
+	assert(tshirtSize(size_cms) == expectedSize);
+}
+
+int main() {
+	testTshirtSize(30,'\0');
+	testTshirtSize(37,'S');
+	testTshirtSize(38,'S');
+	testTshirtSize(40,'M');
+	testTshirtSize(42,'L');
+	testTshirtSize(49,'\0');
+	return 0;
+}
